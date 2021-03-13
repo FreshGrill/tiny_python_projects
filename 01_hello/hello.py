c8dbd3734c77db8eea668c26a5afc7ad5769a05e
Kevin <ktraster@freshgrillfoods.com>
2021-03-12 21:35:52 -0800

    finished chap 1

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python3
-# Purpose: Say hello
-print('Hello, World!')
+"""
+Author: Kevin Traster <ktraster@freshgrillfoods.com>
+Purpose:Say Hello
+"""
+import argparse
+
+
+# -------------------------------
+def get_args():
+    """Get the command-line arguments"""
+    parser = argparse.ArgumentParser(description='Say Hello')
+    parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
+    return parser.parse_args()
+
+
+# -------------------------------
+def main():
+    """Main program"""
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
+# ------------------
+if __name__ == '__main__':
+    main()
